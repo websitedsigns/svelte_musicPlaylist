e174e47176f351d191dca444019a95b99db6b224
websitedsigns <websitedsigns@gmail.com>
2023-09-20 11:52:26 +0100

    update state

--- svelte_music_playlist/src/App.svelte
+++ svelte_music_playlist/src/App.svelte
@@ -1,81 +1,49 @@
 <script lang="ts">
-
   import PlaylistItem from './lib/PlaylistItem.svelte';
-
-
-
   const tracks = [
 
     {
-
       id: 1,
-
       title: 'Track title',
-
       artist: 'Artist Name',
-
       album: 'Album Name',
-
       length: '4:23',
-
     },
 
     {
-
       id: 2,
-
       title: 'Track title',
-
       artist: 'Artist Name',
-
       album: 'Album Name',
-
       length: '4:23',
-
     },
 
     {
-
       id: 3,
-
       title: 'Track title',
-
       artist: 'Artist Name',
-
       album: 'Album Name',
-
       length: '4:23',
-
     },
 
   ] as Track[];
-
   let currentTrack = -1;
-
 </script>
 
 
 
 <div class="min-h-screen bg-slate-800 text-white">
-
   <main class="max-w-screen-md mx-auto">
-
     <header class="bg-pink-600 text-2xl font-bold p-3">
-
       <h1>Sounds Good! - Epic Playlist</h1>
-
     </header>
-
     <ol class="border-4 border-pink-600">
-
       {#each tracks as track}
-
-        <PlaylistItem track={track} state={track.id === currentTrack ? 'playing' : 'paused'}/>
-
+        <PlaylistItem
+          track={track}
+          state={track.id === currentTrack ? 'playing' : 'paused'}
+          on:click={() => { track.id === currentTrack ? currentTrack = -1 : currentTrack = track.id; }}/>
       {/each}
-
     </ol>
-
   </main>
-
 </div>
--- svelte_music_playlist/src/lib/PlaylistItem.svelte
+++ svelte_music_playlist/src/lib/PlaylistItem.svelte
@@ -1,53 +1,31 @@
 <script lang="ts">
-
+    import { createEventDispatcher } from 'svelte';
     import SvgIcon from '@jamescoyle/svelte-icon';
-  
     import { mdiMusic, mdiPlay, mdiPause } from '@mdi/js';
-  
-  
-  
+    
     export let track: Track;
-  
     export let state: 'playing' | 'paused';
   
-  
+    const dispatch = createEventDispatcher();
   
     function playPauseTrack() {
-  
-      alert('Clicked!');
-  
+      dispatch('click');
     }
-  
   </script>
   
-  
-  
   <li class="flex flex-row px-3 py-2 space-x-4 mb-2">
-  
     <div class="w-12 h-12 text-neutral-400 p-2 border border-neutral-400" aria-hidden="true">
-  
       <SvgIcon type="mdi" path={mdiMusic} />
-  
     </div>
-  
     <div class="flex-1">
-  
       <div class="flex flex-row">
-  
         <h2 class="flex-1 font-bold">{track.title}</h2>
-  
         <p class="text-neutral-200">{track.length}</p>
-  
       </div>
-  
       <p>{track.artist} - {track.album}</p>
-  
     </div>
-  
-    <button on:click={playPauseTrack} class="w-12 h-12 p-2" aria-label="{state === 'playing' ? 'Pause' : 'Start'} playing this track">
-  
+
+    <button on:click={playPauseTrack} class="w-12 h-12 p-2" aria-label="Pause playing this track">
       <SvgIcon type="mdi" path={state === 'playing' ? mdiPause : mdiPlay} />
-  
     </button>
-  
   </li>
